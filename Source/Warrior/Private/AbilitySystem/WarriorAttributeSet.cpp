ea34fe69600df61508fe1a962fd72948c09f5b89
SnowyOwl <48579934+snowyowl981@users.noreply.github.com>
2025-06-02 18:20:41 +0900

    캐릭터 속성값 추가
    적 캐릭터 무기 스케일 1->1.5로 조정
    최대, 현재 체력 및 분노
    공격력 방어력 생성 및 생성자에서 초기화 기능 추가

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -3,3 +3,13 @@
 
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+	// 생성 시 속성값 초기화
+	InitCurrentHealth(1.f);
+	InitMaxHealth(1.f);
+	InitCurrentRage(1.f);
+	InitMaxRage(1.f);
+	InitAttackPower(1.f);
+	InitDefensePower(1.f);
+}
--- Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "WarriorAttributeSet.generated.h"
+
+// Getter, Setter, Initter Helper Function (UAttributeSet) 포함
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +21,38 @@ UCLASS()
 class WARRIOR_API UWarriorAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	// 생성자
+	UWarriorAttributeSet();
+
+	// 현재 체력
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentHealth)
+
+	// 최대 체력
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxHealth)
+
+	// 현재 분노
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentRage)
+
+	// 최대 분노
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxRage)
+
+	// 공격력
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, AttackPower)
+
+	// 방어력
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, DefensePower)
 };
