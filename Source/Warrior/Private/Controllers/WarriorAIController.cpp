2618989882fec953a4f0ba4166ef06413c06a6a4
SnowyOwl <48579934+snowyowl981@users.noreply.github.com>
2025-09-14 00:36:01 +0900

    적 AI 추가
    1. 적 AI BB, BT 추가
    2. 맵에 Nav Mesh Bounds Volume 추가
    3. 적이 플레이어를 보고 따라오도록 설정

--- Source/Warrior/Private/Controllers/WarriorAIController.cpp
+++ Source/Warrior/Private/Controllers/WarriorAIController.cpp
@@ -3,6 +3,9 @@
 
 #include "Controllers/WarriorAIController.h"
 #include "Navigation/CrowdFollowingComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 #include "WarriorDebugHelper.h"
 
@@ -16,5 +19,62 @@ AWarriorAIController::AWarriorAIController(const FObjectInitializer& ObjectIniti
 	if (UCrowdFollowingComponent* CrowdComp = Cast<UCrowdFollowingComponent>(GetPathFollowingComponent()))
 	{
 		Debug::Print(TEXT("CrowdFollowingComponent valid"), FColor::Green);
+	}
+
+	// AISenseConfig_Sight 생성 및 초기화
+	AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("EnemySenseConfig_Sight");
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectEnemies = true;		// 적 감지 활성화
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectFriendlies = false;	// 아군 감지 비활성화
+	AISenseConfig_Sight->DetectionByAffiliation.bDetectNeutrals = false;	// 중립 감지 비활성화
+	
+	// 시야 설정
+	AISenseConfig_Sight->SightRadius = 5000.f;								// 시야 범위
+	AISenseConfig_Sight->LoseSightRadius = 0.f;								// 대상 탐지 최대 시야 범위
+	AISenseConfig_Sight->PeripheralVisionAngleDegrees = 360.f;				// 탐지 시야각
+
+	// EnemyPerceptionComponent 생성 및 초기화
+	EnemyPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("EnemyPerceptionComponent");
+	
+	// 시각 감지 설정값을 PerceptionComponent에 등록
+	EnemyPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
+	
+	// 지배적인 감지 방식(우선순위 Sense)을 시각(Sight)으로 설정
+	EnemyPerceptionComponent->SetDominantSense(UAISenseConfig_Sight::StaticClass());
+	
+	// 타겟 인식 상태가 바뀔 때 호출될 델리게이트 바인딩 (OnEnemyPerceptionUpdated 함수 실행)
+	EnemyPerceptionComponent->OnTargetPerceptionUpdated.AddUniqueDynamic(this, &ThisClass::OnEnemyPerceptionUpdated);
+
+	// TeamId 설정
+	SetGenericTeamId(FGenericTeamId(1));
+}
+
+// GetTeamAttitude 인터페이스 함수
+ETeamAttitude::Type AWarriorAIController::GetTeamAttitudeTowards(const AActor& Other) const
+{
+	// 체크할 폰, 팀 에이전트 인터페이스 캐스팅
+	const APawn* PawnToCheck = Cast<const APawn>(&Other);
+	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<const IGenericTeamAgentInterface>(PawnToCheck->GetController());
+
+	// TeamId가 다른 경우 적대, 같은 경우 우호
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	{
+		return ETeamAttitude::Hostile;
+	}
+
+	return ETeamAttitude::Friendly;
+}
+
+// 타겟 인식 상태가 바뀔 때 호출될 델리게이트
+void AWarriorAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	// 타겟 감지 시
+	if (Stimulus.WasSuccessfullySensed() && Actor)
+	{
+		// 블랙보드 컴포넌트 할당 및 if 체크
+		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		{
+			// 적 인식 이용해 블랙보드 키 값 설정
+			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+		}
 	}
 }
--- Source/Warrior/Public/Controllers/WarriorAIController.h
+++ Source/Warrior/Public/Controllers/WarriorAIController.h
@@ -6,6 +6,8 @@
 #include "AIController.h"
 #include "WarriorAIController.generated.h"
 
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
 /**
  * 
  */
@@ -15,6 +17,23 @@ class WARRIOR_API AWarriorAIController : public AAIController
 	GENERATED_BODY()
 
 public:
+	// 생성자
 	AWarriorAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer());
+
+	//~ Begin IGenericTeamAgentInterface Interface.
+	virtual ETeamAttitude::Type GetTeamAttitudeTowards(const AActor& Other) const override;
+	//~ End IGenericTeamAgentInterface Interface.
 	
+protected:
+	// 시각, 청각 감지 기능뿐만 아니라 손상 등 다양한 감지 기능 지원 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAIPerceptionComponent* EnemyPerceptionComponent;
+
+	// 시야 감지 파라미터 설정
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UAISenseConfig_Sight* AISenseConfig_Sight;
+
+	// 타겟 인식 상태가 바뀔 때 호출될 델리게이트
+	UFUNCTION()
+	virtual void OnEnemyPerceptionUpdated( AActor* Actor, FAIStimulus Stimulus);
 };
