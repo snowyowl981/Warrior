15837da26c48196389875e58143097af0bc7f1aa
SnowyOwl <snowyowl981@gmail.com>
2025-12-24 20:46:11 +0900

    보스 사망 시 체력 바 제거
    1. 보스 캐릭터 위젯 생성 시 등록하는 기능 추가
    2. 적 캐릭터 사망 시 등록된 위젯이 있는 경우 제거하는 기능 추가

--- Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
+++ Source/Warrior/Private/Components/UI/EnemyUIComponent.cpp
@@ -2,4 +2,28 @@
 
 
 #include "Components/UI/EnemyUIComponent.h"
+#include "Widgets/WarriorWidgetBase.h"
 
+void UEnemyUIComponent::RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister)
+{
+	// 등록할 적 위젯을 배열에 추가
+	EnemyDrawnWidgets.Add(InWidgetToRegister);
+}
+
+void UEnemyUIComponent::RemoveEnemyDrawnWidgetIfAny()
+{
+	// 배열이 비어있는 경우 함수 종료
+	if (EnemyDrawnWidgets.IsEmpty())
+	{
+		return;
+	}
+	
+	// 배열 돌면서 생성된 위젯 제거
+	for (UWarriorWidgetBase* DrawnWidget : EnemyDrawnWidgets)
+	{
+		if (DrawnWidget)
+		{
+			DrawnWidget->RemoveFromParent();
+		}
+	}
+}
--- Source/Warrior/Public/Components/UI/EnemyUIComponent.h
+++ Source/Warrior/Public/Components/UI/EnemyUIComponent.h
@@ -6,6 +6,7 @@
 #include "Components/UI/PawnUIComponent.h"
 #include "EnemyUIComponent.generated.h"
 
+class UWarriorWidgetBase;
 /**
  * 
  */
@@ -13,5 +14,18 @@ UCLASS()
 class WARRIOR_API UEnemyUIComponent : public UPawnUIComponent
 {
 	GENERATED_BODY()
+	
+public:
+	// 그린 적 위젯 배열 등록
+	UFUNCTION(BlueprintCallable)
+	void RegisterEnemyDrawnWidget(UWarriorWidgetBase* InWidgetToRegister);
+	
+	// 그린 적 위젯이 있는 경우 제거
+	UFUNCTION(BlueprintCallable)
+	void RemoveEnemyDrawnWidgetIfAny();
+	
+private:
+	// 이미 그린 적 위젯 배열
+	TArray<UWarriorWidgetBase*> EnemyDrawnWidgets;
 	
 };
